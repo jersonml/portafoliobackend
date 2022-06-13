9c58ee364723a74c38fe77d827527df15fd253a8
jerson <jerson.ml820@hotmail.com>
2022-06-13 19:32:15 -0400

    config email

--- config/settings/production.py
+++ config/settings/production.py
@@ -125,9 +125,14 @@ INSTALLED_APPS += ["anymail"]  # noqa F405
 # https://anymail.readthedocs.io/en/stable/esps/mailgun/
 EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
 ANYMAIL = {
-    "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
-    "MAILGUN_SENDER_DOMAIN": env("MAILGUN_DOMAIN"),
-    "MAILGUN_API_URL": env("MAILGUN_API_URL", default="https://api.mailgun.net/v3"),
+   "SPARKPOST_API_KEY": "a947d8285e3f47bdca8c2f03a83407183f293849",
+    "SPARKPOST_API_URL": "https://api.sparkpost.com/api/v1",
+    'SPARKPOST_SENDER_DOMAIN': "https://portafolioback-jerson.herokuapp.com/",
+
+    "SEND_DEFAULTS": {
+        "track_clicks": False,
+        "track_opens": False,
+    },
 }
 
 # Collectfast
